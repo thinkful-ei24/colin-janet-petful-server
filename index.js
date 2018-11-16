40ddad6080fa6775b8b0e8463a5ed8c64da77374
Colin Rupp <colin@thinkful.com>
2018-11-16 08:16:23 -0800

    adds dummy data

--- index.js
+++ index.js
@@ -22,6 +22,20 @@ app.use(
   })
 );
 
+//TODO make cat router
+//TODO make dog router
+
+
+app.use((err, req, res, next) => {
+  if(err.status) {
+    const errBody = Object.assign({}, err, { message: err.message });
+    res.status(err.status).json(errBody);
+  } else {
+    console.error(err);
+    res.status(500).json({ message: 'the server shit the bed' });
+  }
+});
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
